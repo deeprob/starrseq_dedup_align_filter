66a5eda16855232b5f280b1e5dadca988b8fb82e
Deepro Banerjee <dzb5732@durga.bx.psu.edu>
2023-01-18 10:11:02 -0500

    unix-warning-message-*]-missing*-edited

--- src/shell_scripts/3_filter_reads.sh
+++ src/shell_scripts/3_filter_reads.sh
@@ -23,7 +23,7 @@ done
 for rep in "${BIOL_REPS[@]}"
 do
 
-    if [ -z $ROI_FILE]
+    if [ -z $ROI_FILE ]
     then
         samtools index ${INPUT_PREFIX}_${rep}.bam
         samtools view -F 2828 -f 2 -q 30 -@ 64 -o ${OUTPUT_PREFIX}_${rep}.bam ${INPUT_PREFIX}_${rep}.bam chr1 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr2 chr20 chr21 chr22 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chrX chrY
